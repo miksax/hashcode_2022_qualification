a370bcf2d63fe3723cf0101cb496febefd78023c
Martin Miksanik <martin.miksanik@vacuumlabs.com>
2022-02-24 20:30:04 +0100

    Init 3

--- project.py
+++ project.py
@@ -11,7 +11,7 @@ class Role:
 @dataclass
 class Person:
     name: str
-    role: List[Role]
+    roles: List[Role]
 
 
 class Project:
@@ -21,25 +21,86 @@ class Project:
     roles: List[Role]
 
 
+class Progress:
+    people: List[Person]
+    project: Project
+    start: int
+
+
 @dataclass
 class Solver:
     step: int
-    projects: List[Project]
     people: List[Person]
+
+    projects: List[Project]
+    progress: List[Progress]
+
     solvable: bool
+    score: int
+    day: int
 
     def __init__(self, people: List[Person], projects: List[Project]):
         pass
 
-    def next(self) -> Solver:
-        pass
+    def copy(self):
+        Solver(
+            step=self.step,
+            projects=self.projects,
+            people=[
+                Person(
+                    name=person.name,
+                    roles=[
+                        Role(name=role.name, level=role.level)
+                        for role in person.roles
+                    ]
+                )
+                for person in self.people
+            ],
+            progress=self.progress,
+            solvable=self.solvable,
+            score=self.score,
+            day=self.day,
+        )
+
+    def run(self) -> Solver:
+
+        best = None
+        for solving in self.next():
+            result = solving.run()
+            if best is None or result.score > best and result.solvable:
+                best = result
+
+        if best is not None:
+            return best
+
+        self.solvable = False
+        return self
 
     def next_day(self) -> Solver:
-        pass
+        """ Probalbly obsolete """
 
-    def run(self, project: Project, people: List[Person]) -> Solver:
+        return self.copy()
 
-        return Solver()
+    def next(self,) -> Solver:
+        """ will pick up project and pepople """
+        # project: Project, people: List[Person]
+        solving = self.copy()
+        solving.step += 1 #etc.
+        return solving
 
-    def get_project(self):
+    def get_projects(self) -> Generator[Project]:
+        """
+        Return projects what is possible to work on
+        """
+
+    def get_solvers(self, project: Project) -> Generator[List[Person]]:
+        """
+        Get sovlers what can work on project
+        """
+        for person in self.get_free_people():
+            person
+
+        return []
+
+    def get_free_people(self):
         pass
